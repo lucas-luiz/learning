8b0eab4ba4be8f7c9de136365b70c5a5e27df931
lucas-luiz <lucas.ldsilva10@gmail.com>
2022-07-04 23:51:47 -0300

    structs

---
+++ C/dataStructures/struct/ex_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct
+{
+    int cod;
+    float price;
+} Product;
+
+void verifyProduct(int productIndex, Product *products)
+{
+    printf(" %i", products[productIndex].cod);
+}
+
+int main()
+{
+    int i;
+    Product products[5];
+    for (i = 0; i < 2; i++)
+    {
+        scanf(" %d%f", &products[i].cod, &products[i].price);
+    }
+    verifyProduct(1, products);
+    return 0;
+}
