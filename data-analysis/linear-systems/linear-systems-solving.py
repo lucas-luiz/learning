b9bd3dd506d37433f53503b552adec48a9f33c2f
lucas-luiz <lucas.ldsilva10@gmail.com>
2023-07-11 23:53:31 -0300

    linear systems

--- data-analysis/linear-systems/linear-systems-solving.py
+++ data-analysis/linear-systems/linear-systems-solving.py
@@ -1,73 +1,74 @@
+#Lucas Luiz da Silva
+#2022008662
 import numpy as np
 
-def main():
-    A = np.zeros((10, 10))
-    B = np.zeros((10, 11))
-    b = np.zeros(10)
-    x = np.zeros(10)
-    Laux = np.zeros(10)
-    i = j = k = n = l = 0
-    m = S = 0.0
+i = j = k = n = l = 0
+m = S = 0.0
 
-    # leitura da ordem da matriz
-    print("\nEntre com a ordem da matriz de coeficientes do sistema linear.")
-    n = int(input("\nn = "))
+print("\nEntre com a ordem da matriz")
+n = int(input("\nn = "))
 
-    # leitura dos dados da matriz
-    print("\nEntre com os elementos da matriz de coeficientes do sistema linear.")
-    print("\nEntre com as linhas.")
-    for i in range(n):
-        print("\n")
-        for j in range(n):
-            A[i][j] = float(input("A=[{}][{}] = ".format(i, j)))
+A = np.zeros((n, n))
+B = np.zeros((n, n+1))
+b = np.zeros(n)
+x = np.zeros(n)
+Laux = np.zeros(n)
 
-    # leitura do vetor
-    print("\nEntre com os elementos dos termos independentes de cada equação.")
-    for i in range(n):
-        b[i] = float(input("b[{}] = ".format(i)))
-
-    # Montando a matriz expandida
-    B[:n, :n] = A
-
-    for i in range(n):
-        B[i][n] = b[i]
-
-    # impressão da matriz expandida
+print("\nEntre com as linhas")
+for i in range(n):
     print("\n")
-    for i in range(n):
-        print("\n")
-        for j in range(n + 1):
-            print("B=[{}][{}] = {:2.5f}".format(i, j, B[i][j]))
+    for j in range(n):
+        A[i][j] = float(input("A=[{}][{}] = ".format(i, j)))
 
-    # escalonamento da matriz expandida
-    for k in range(n - 1):
-        for i in range(k + 1, n):
-            m = B[i][k] / B[k][k]
-            B[i][k] = 0
-            for j in range(k + 1, n + 1):
-                B[i][j] = B[i][j] - (m * B[k][j])
+print("\nEntre com os termos independentes")
+for i in range(n):
+    b[i] = float(input("b[{}] = ".format(i)))
 
-    # Impressão Matriz escalonada
-    print("\nMatriz escalonada.\n")
-    for i in range(n):
-        print("\n")
-        for j in range(n + 1):
-            print("B=[{}][{}] = {:2.5f}".format(i, j, B[i][j]))
+B[:n, :n] = A
 
-    # Resolução do sistema triangular
-    x[n - 1] = B[n - 1][n] / B[n - 1][n - 1]
+for i in range(n):
+    B[i][n] = b[i]
 
-    for i in range(n - 2, -1, -1):
-        S = 0
-        for k in range(i + 1, n):
-            S = S + B[i][k] * x[k]
-        x[i] = (-S + B[i][n]) / B[i][i]
+print("\nMatriz.\n")
+for i in range(n):
+    print(B[i])
 
-    # Impressão da solução
-    print("\nSolução do sistema.\n")
-    for i in range(n):
-        print("x=[{}] = {}".format(i, x[i]))
+for k in range(n - 1):
+    max_index = k
+    max_value = abs(B[k][k])
+
+    for i in range(k + 1, n):
+        if abs(B[i][k]) > max_value:
+            max_index = i
+            max_value = abs(B[i][k])
+
+    B[[k, max_index]] = B[[max_index, k]]
+
+print("\nMatriz após pivoteamento.\n")
+for i in range(n):
+    print(B[i])
+
+for k in range(n - 1):
+    for i in range(k + 1, n):
+        m = B[i][k] / B[k][k]
+        B[i][k] = 0
+        for j in range(k + 1, n + 1):
+            B[i][j] = B[i][j] - (m * B[k][j])
+
+print("\nMatriz escalonada.\n")
+for i in range(n):
+    print(B[i])
+
+x[n - 1] = B[n - 1][n] / B[n - 1][n - 1]
+
+for i in range(n - 2, -1, -1):
+    S = 0
+    for k in range(i + 1, n):
+        S = S + B[i][k] * x[k]
+    x[i] = (-S + B[i][n]) / B[i][i]
+
+print("\nSolução do sistema.\n")
+for i in range(n):
+    print("x=[{}] = {}".format(i, x[i]))
 
 
-if __name__ == "__main__":
-    main()
